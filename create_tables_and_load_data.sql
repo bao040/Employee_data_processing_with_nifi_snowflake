3166c93e9316fda727458d94cf2f206ff31ff41e
bao040 <119292750+bao040@users.noreply.github.com>
2024-12-21 01:52:08 +0700

    Update and rename create_table.sql to create_tables_and_load_data.sql

--- create_tables_and_load_data.sql
+++ create_tables_and_load_data.sql
@@ -45,3 +45,38 @@ create or replace table employee_logs (
 );
 
 create or replace stream employee_changes on table employee;
+
+-- Load data from s3 with snowpipe
+
+desc integration EMPLOYEE_STORAGE_INTEGRATION;
+
+CREATE OR REPLACE FILE FORMAT employee_nifi_scd.stages_pipes.csv_file_format
+TYPE = 'CSV'
+FIELD_DELIMITER = ','
+FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+SKIP_HEADER = 1
+NULL_IF = ('NULL', 'null')
+--DATE_FORMAT = 'MM/DD/YYYY'
+EMPTY_FIELD_AS_NULL = TRUE
+;
+
+create or replace stage employee_nifi_scd.stages_pipes.employee_stage
+url = 's3://employee-nifi-snowflake/'
+storage_integration = EMPLOYEE_STORAGE_INTEGRATION
+file_format = employee_nifi_scd.stages_pipes.csv_file_format
+;
+
+CREATE OR REPLACE PIPE employee_nifi_scd.stages_pipes.employee_pipe
+AUTO_INGEST = TRUE
+AS 
+COPY INTO employee_nifi_scd.public.employee_raw
+FROM @employee_nifi_scd.stages_pipes.employee_stage;
+
+show pipes;
+
+select * from employee_nifi_scd.public.employee_raw;
+
+
+
+
+
